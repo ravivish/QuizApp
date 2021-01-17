08b0fd94d4a86bf96c5715fe3ccb09096e214494
Ravi Vishwakarma <ravivishwakarma825@gmail.com>
2021-01-17 23:18:37 +0530

    New Questions added

--- app.js
+++ app.js
@@ -5,18 +5,17 @@ const getAnswer = (key) => {
         question1: 1,
         question2: 2,
         question3: 3,
-        question4: 4,
-        question5: 2,
+        question4: 3,        
     }
     return answers[key];
 }
 
 const getQuestion = (key) => {
     const questionBank = {
-        question1 : `Q1.What is First color of flag?\n1.Red\n2.Green\n3.Blue\n4.Yellow\n`,
-        question2 : `Q2.What is Second color of flag?\n1.Red\n2.Green\n3.Blue\n4.Yellow\n`,
-        question3 : `Q3.What is Third color of flag?\n1.Red\n2.Green\n3.Blue\n4.Yellow\n`,
-        question4 : `Q4.What is color of flag?\n1.Red\n2.Green\n3.Blue\n4.Yellow\n`
+        question1 : `Q1. Which of the following is a single global function defined in the jQuery library ?\n1. jQuery()\n2. $()\n3. Queryanalysis()\n4. None of the mentioned\n`,
+        question2 : `Q2. Which jQuery method is used to hide selected elements ?\n1. hidden()\n2. hide()\n3. visible(false)\n4. display(none)\n`,
+        question3 : `Q3. $.foo() is equivalent to ?\n1. javascript.foo()\n2. document.foo()\n3. jQuery.foo()\n4. None of the above\n`,
+        question4 : `Q4. Which built-in method returns the character at the specified index??\n1. characterAt()\n2. getCharAt()\n3. charAt()\n4. None of the above\n`
     }
     return questionBank[key];
 }
@@ -32,14 +31,14 @@ for (let i = 1; i <= 4; ) {
         let validAns = parseInt(ans);
         if(validAns===getAnswer(`question${i}`)){
             usermarks += 5;
-            console.log(`\nCorrect Answer\n`);
+            console.log(`Correct Answer\n`);
         }else{
-            console.log(`\nWrong answer!\nCorrect option is ${getAnswer(`question${i}`)}\n`);
+            console.log(`Wrong answer!\nCorrect option is ${getAnswer(`question${i}`)}\n`);
         }
         i++;
     }else{
-        console.log('\nplease choose valid option\n');
+        console.log('please choose valid option\n');
     }
 }
-console.log(`\nQuiz over and\n`);
-console.log(`\nYou got ${usermarks} out of ${totalmarks}\n`);
+console.log(`Quiz over.`);
+console.log(`You got ${usermarks} out of ${totalmarks}.`);
